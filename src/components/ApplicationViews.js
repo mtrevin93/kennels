3277fb556e578092e5f7a2cde0dc56dc4f302cb7
mtrevin93 <mtrevin93@gmail.com>
2021-07-08 16:10:58 -0400

    refactored code to pull from API instead of dummy data. added animals and customers this way

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
+import { LocationCard } from "./location/LocationCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
+import { CustomerProvider } from "./customer/CustomerProvider"
+import { CustomerList } from "./customer/CustomerList"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/animals */}
+            <Route exact path ="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
+            <Route path="/locations">
+                <LocationCard />
+            </Route>
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+            <CustomerProvider>
+                <Route exact path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
+        </>
+    )
+}
